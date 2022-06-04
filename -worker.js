2f39c6822b1b8b941729e749b3c1d5daac645099
lkkuh <101034484+lkkuh@users.noreply.github.com>
2022-06-04 08:39:38 +0000

    Update -worker.js

--- -worker.js
+++ -worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'd5h8f.herokuapp.com'
+      url.hostname = 'k8v5h.herokuapp.com'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
